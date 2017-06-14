532e2adbc459d3adb189a63f0d1e8b35cf69bf41
Ian Hunter <Ian Hunter>
2017-06-13 17:05:49 -0700

    Chapters Menu Toggles

---
+++ Assets/Scripts/MainMenuScripts/ColorSlider.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorSlider : MonoBehaviour {
+
+    public float red, green, blue;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void RedChanged(float value)
+    {
+        this.red = value;
+        SetColor();
+    }
+
+    public void BlueChanged(float value)
+    {
+        this.blue = value;
+        SetColor();
+    }
+
+    public void GreenChanged(float value)
+    {
+        this.green = value;
+        SetColor();
+    }
+
+    public void SetColor()
+    {
+        GetComponent<Renderer>().material.color = new Color(red, green, blue);
+    }
+}
---
+++ Assets/Scripts/MainMenuScripts/PanelManagerUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelManagerUI : MonoBehaviour {
+
+
+    public GameObject chaptersPanel;
+
+	// Use this for initialization
+	void Start () {
+        chaptersPanel.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ToggleSettingsPanel()
+    {
+
+        bool active;
+        active = chaptersPanel.activeSelf == true ? false : true;
+        chaptersPanel.SetActive(active);
+    }
+
+    public void HideSettingsPanel()
+    {
+        chaptersPanel.SetActive(false);
+    }
+}
+
